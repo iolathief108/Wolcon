ee133ee32a89e0896ad0eb9ace61ad07d2b07e59
Hafil Desktop <iolathief108@gmail.com>
2021-04-26 20:29:27 +0530

    fix second screen scroll up trigger the task view

--- wolcon/src/Wolcon.cpp
+++ wolcon/src/Wolcon.cpp
@@ -3,8 +3,7 @@
 #include <tchar.h>
 #include <thread>
 
-void buttonPress(BYTE vkCode)
-{
+void keyDownUp(BYTE vkCode) {
     // Simulate a key press
     keybd_event(vkCode,
                 0x45,
@@ -19,39 +18,46 @@ void buttonPress(BYTE vkCode)
 
 }
 
-void volumeButton(bool up)
-{
-    if (up)
-        buttonPress(VK_VOLUME_UP);
-    else
-        buttonPress(VK_VOLUME_DOWN);
+void keyDown(BYTE vkCode) {
+    // Simulate a key press
+    keybd_event(vkCode,
+                0x45,
+                KEYEVENTF_EXTENDEDKEY | 0,
+                0);
+}
+
+void keyUp(BYTE vkCode) {
+    // Simulate a key release
+    keybd_event(vkCode,
+                0x45,
+                KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
+                0);
 }
 
 POINT p{};
 
-bool           taskViewPerformed;
+bool taskViewPerformed;
 unsigned short count;
-std::thread    *tvThread;
-bool           tView;
+std::thread *tvThread;
+bool tView;
 
-void taskViewRunner()
-{
-    std::cout << "in" << std::endl;
-    tView      = true;
+void taskViewRunner() {
+    tView = true;
     for (count = 0; count < 335; ++count) {
         Sleep(1);
     }
-    std::cout << "out" << std::endl;
     taskViewPerformed = false;
 }
 
-bool wheelHandle(bool volUp)
-{
+bool wheelHandle(bool volUp) {
     GetCursorPos(&p);
     if (p.y > (GetSystemMetrics(SM_CYSCREEN) - 3)) {
-        volumeButton(volUp);
+        if (volUp)
+            keyDownUp(VK_VOLUME_UP);
+        else
+            keyDownUp(VK_VOLUME_DOWN);
         return true;
-    } else if (volUp && p.y<(GetSystemMetrics(SM_CYSCREEN) - 3) && p.x>(GetSystemMetrics(SM_CXSCREEN) - 3)) {
+    } else if (volUp && p.y < 3 && p.x > GetSystemMetrics(SM_CXSCREEN) - 3 && p.x < GetSystemMetrics(SM_CXSCREEN)) {
         count = 0;
         if (!taskViewPerformed) {
             keybd_event(VK_LWIN,
@@ -84,14 +90,13 @@ bool wheelHandle(bool volUp)
     return false;
 }
 
-HHOOK          mouseHook;
+HHOOK mouseHook;
 MSLLHOOKSTRUCT *mStruct;
 
 bool topLeftConer;
 bool mButtonPress;
 
-LRESULT CALLBACK mouseProc(int nCode, WPARAM wParam, LPARAM lParam)
-{
+LRESULT CALLBACK mouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
 
     if (nCode == HC_ACTION) {
 
@@ -102,7 +107,7 @@ LRESULT CALLBACK mouseProc(int nCode, WPARAM wParam, LPARAM lParam)
         }
 
         if (wParam == WM_MOUSEMOVE) {
-            mStruct      = (MSLLHOOKSTRUCT *) lParam;
+            mStruct = (MSLLHOOKSTRUCT *) lParam;
             topLeftConer = static_cast<int>(mStruct->pt.x) <= 0 && static_cast<int>(mStruct->pt.y) <= 0;
         }
 
@@ -120,12 +125,15 @@ LRESULT CALLBACK mouseProc(int nCode, WPARAM wParam, LPARAM lParam)
     return CallNextHookEx(mouseHook, nCode, wParam, lParam);
 }
 
-int CALLBACK  WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
-{
-    topLeftConer      = false;
-    mButtonPress      = false;
+#ifdef CONSOLE
+int main() {
+#else
+int CALLBACK  WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+#endif
+    topLeftConer = false;
+    mButtonPress = false;
     taskViewPerformed = false;
-    tView             = false;
+    tView = false;
 
     mouseHook = SetWindowsHookEx(WH_MOUSE_LL, mouseProc, 0, 0);
 
