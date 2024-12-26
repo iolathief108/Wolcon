237fe249264ada82720de39be57e7345ba28341d
Hafil <iolathief108@gmail.com>
2024-12-27 03:02:49 +0530

    no comments

--- buildVSPro.bat
+++ buildVSPro.bat
@@ -1,4 +1,4 @@
 @ECHO off
 REM G- generator name S- source dir B- build dir
-cmake -G "Visual Studio 16 2019" -S "." -B ".\VS-cmake-build-debug"
+cmake -G "Visual Studio 2022" -S "." -B ".\VS-cmake-build-debug"
 pause
